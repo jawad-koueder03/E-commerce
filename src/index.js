5119a9d4d71ab94b7f8e095ad7ef1a109a6a1af5
jawad-koueder03 <jawad.kou.03@gmail.com>
2026-09-25 20:03:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // === تقديم التطبيق ===
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/E-commerce">
       <App />
     </BrowserRouter>
   </React.StrictMode>
